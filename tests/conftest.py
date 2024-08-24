9c3af66345ee301bd90ca6aa956508356ab1a606
Dea1Mak3r <mr.makintosh998@gmail.com>
2024-08-24 17:09:55 +0300

    Добавил Headless

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 
 
 @pytest.fixture()
 def browser():
-    browser = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless=new")
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(10)
     return browser
 
